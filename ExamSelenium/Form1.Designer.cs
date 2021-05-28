db9bc87c3f9f7ba192c5f829b8a828430a1baaf4
one <2017253020@yonsei.ac.kr>
2021-05-28 16:52:44 +0900

    first

---
+++ ExamSelenium/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ExamSelenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TEST = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TEST
+            // 
+            this.TEST.Location = new System.Drawing.Point(13, 176);
+            this.TEST.Margin = new System.Windows.Forms.Padding(4);
+            this.TEST.Name = "TEST";
+            this.TEST.Size = new System.Drawing.Size(254, 82);
+            this.TEST.TabIndex = 4;
+            this.TEST.Text = "로그인 버튼 클릭";
+            this.TEST.UseVisualStyleBackColor = true;
+            this.TEST.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 351);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TEST);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TEST;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ExamSelenium/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace ExamSelenium
+{
+    public partial class Form1 : Form
+    {
+        protected ChromeDriverService _driverService = null;
+        protected ChromeOptions _options = null;
+        protected ChromeDriver _driver = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                _driverService = ChromeDriverService.CreateDefaultService();
+                _driverService.HideCommandPromptWindow = true;
+
+                _options = new ChromeOptions();
+                _options.AddArgument("disable-gpu");
+            }
+            catch(Exception exc)
+            {
+                Trace.WriteLine(exc.Message);
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string id = textBox1.Text;
+                string pw = textBox2.Text;
+
+                _driver = new ChromeDriver(_driverService, _options);
+                id = "2017253020";
+                pw = "980408";
+//                _driver.Navigate().GoToUrl("https://open.yonsei.ac.kr/passni/sso/coursemosLogin.php?username="+id+"&password="+pw+"&ssoGubun=Login");
+                _driver.Navigate().GoToUrl("https://m.blog.naver.com/yug311861/221835061758");
+
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+               _driver.FindElement(By.XPath("//*[@id='body']/div[4]/div[1]/h1/a/span")).Click();
+  /*              var li = title.FindElements(By.CssSelector(".course-title"));
+                foreach (var course in li) 
+                {
+                    var course_name = course.FindElement(By.XPath("/h3")).Text ;
+                    Trace.WriteLine(course_name);
+                }
+ */           }
+            catch (Exception exc)
+            {
+                Trace.WriteLine(exc.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _driver.Quit();
+        }
+    }
+}
